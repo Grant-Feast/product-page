ff50a8e7a59e14277b6d4fa7c75f5e67f66412c9
Grant Feast <feast.grant@gmail.com>
2023-04-07 12:51:45 +0100

    Show personalisation text in the personalise element when modal is closed

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // Collapsable menu variales
-const burgerIcon = document.getElementById("burger");
+const burgerIcon = document.getElementById('burger');
+// Color option variables
+const color = document.getElementsByClassName('color-option');
 // Modal variables
 const modal = document.getElementById('modal');
 const openModal = document.getElementById('plus-container')
+const submitBtn = document.getElementById('textSubmit');
+const defaultTag = document.getElementById('default-text');
+// Personalisation element variables
+const plusIcon = document.getElementById('plus-container');
+const clearIcon = document.getElementById('clear-container');
+
 
 // when the burger icon is clicked on mobile screens, the hidden class is added or removed from the main nav
 burgerIcon.addEventListener('click', () => {
@@ -33,3 +41,21 @@ window.onclick = function(event) {
     modal.classList.add('hidden');
   }
 }
+
+// When the user clicks on the submit button, the input value is placed in the personalisation tab
+// At the same time the plus icon will be hidden and the x icon will appear
+submitBtn.addEventListener('click', () => {
+  const userInput = document.getElementById('user-input');
+  defaultTag.innerText = `Personalisation - ${userInput.value}`;
+  modal.classList.add('hidden');
+  plusIcon.classList.add('hidden');
+  clearIcon.classList.add('show');
+})
+
+// When the user clicks on the x icon in the personalisation element, the input text is cleared
+clearIcon.addEventListener('click', () => {
+  defaultTag.innerText = `Add a personalisation`;
+  clearIcon.classList.remove('show');
+  plusIcon.classList.add('show');
+}) 
+  
